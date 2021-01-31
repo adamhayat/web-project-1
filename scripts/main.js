782cd0b0586374946baf802e8eac5c5fe01c4d2b
Adam Hayat <me@adamhayat.ca>
2021-01-30 21:17:11 -0500

    Bug fixes, text updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,15 @@
 
 /* Image changer */
-let myImage = document.querySelector('img'); // this makes querySelector run when myImage is used
+let myImage = document.getElementById('imageChange'); // this makes querySelector run when myImage is used
 
 myImage.onclick = function() {
-let mySrc = myImage.getAttribute('src');
-if(mySrc === 'images/2f0.gif') {
-  myImage.setAttribute('src','images/image2.jpg');
-} else {
-  myImage.setAttribute('src','images/2f0.gif');
-}
-}
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/image2.jpg') {
+    myImage.setAttribute('src','images/2f0.gif');
+  } else {
+    myImage.setAttribute('src','images/image2.jpg');
+  }
+  }
 
 /* Personalized welcome message */
 let myButton = document.querySelector('button');
@@ -21,7 +21,7 @@ function setUserName() {
     setUserName();
   } else {
     localStorage.setItem('name', myName);
-    myHeading.textContent = `Wow! ` + myName + ` thinks that Kish is shit at Catan!`;
+    myHeading.textContent = `Wow! ` + myName + ` thinks that Kish 💩 is shit at Catan!`;
   }
 }
 
@@ -29,7 +29,7 @@ if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   let storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Wow, Kish is really shit at Catan!';
+  myHeading.textContent = 'Wow, Kish 💩 is really shit at Catan! ';
 }
 
 myButton.onclick = function() {
